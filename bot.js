b573444489cb69507210b240daa163338352820b
Jeffrey( Cheuk) Chung <13912201@student.uts.edu.au>
2022-09-29 00:33:02 +1000

    can remove said roles as well

--- bot.js
+++ bot.js
@@ -31,16 +31,39 @@ client.on('messageCreate', message => {
         message.reply('intermediate class role added'); //feedback
     }
 
-     //add beginner role
+     //add advanced role
      if (message.content === '!add-advanced') {
         message.member.roles.add("1024672057855844392"); //add appropriate role ID for advanced role in JASS server
         message.reply('advanced class role added'); //feedback
     }
 
-     //add beginner role
+     //add english/study group role
      if (message.content === '!add-english') {
         message.member.roles.add("1024672075526438922"); //add appropriate role ID for english role in JASS server
         message.reply('english/study group class role added'); //feedback
+    }
+     //remove beginner role
+    if (message.content === '!remove-beginner') {
+        message.member.roles.remove("1024671967846080613"); //add appropriate role ID for beginner role in JASS server
+        message.reply('beginner class role removed'); //feedback
+    }
+
+    //remove intermediate role
+    if (message.content === '!remove-intermediate') {
+        message.member.roles.remove("1024672039585460224"); //add appropriate role ID for intermediate role in JASS server
+        message.reply('intermediate class role removed'); //feedback
+    }
+
+    //remove advanced role
+    if (message.content === '!remove-advanced') {
+        message.member.roles.remove("1024672057855844392"); //add appropriate role ID for advanced role in JASS server
+        message.reply('advanced class role removed'); //feedback
+    }
+
+    //remove english/study group role
+    if (message.content === '!remove-english') {
+        message.member.roles.remove("1024672075526438922"); //add appropriate role ID for english role in JASS server
+        message.reply('english/study group class role removed'); //feedback
     }
 });
 
