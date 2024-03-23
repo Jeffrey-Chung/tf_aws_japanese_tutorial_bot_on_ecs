97df6d271f183eaafcfae6e258ebcd20462fa7ba
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2024-03-24 01:08:42 +1100

    change reference to application name

--- modules/beanstalk/beanstalk.tf
+++ modules/beanstalk/beanstalk.tf
@@ -5,7 +5,7 @@ resource "aws_elastic_beanstalk_application" "japanese_language_bot" {
 
 resource "aws_elastic_beanstalk_application_version" "japanese_language_bot_version" {
   name        = var.beanstalk_version_label
-  application = var.beanstalk_application_name
+  application = aws_elastic_beanstalk_application.japanese_language_bot.name
   description = var.beanstalk_application_description
   bucket      = aws_s3_bucket.jchung_s3_bucket.id
   key         = aws_s3_object.docker_compose_file.id
@@ -13,12 +13,12 @@ resource "aws_elastic_beanstalk_application_version" "japanese_language_bot_vers
 
 resource "aws_elastic_beanstalk_environment" "beanstalk_environment" {
   name                = var.beanstalk_environment_name
-  application         = var.beanstalk_application_name
+  application         = aws_elastic_beanstalk_application.japanese_language_bot.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.2.2 running Docker"
 
   setting {
       namespace = "aws:autoscaling:launchconfiguration"
       name      = "IamInstanceProfile"
-      value     = "aws-elasticbeanstalk-ec2-role"
+      value     = "arn:aws:iam::663790350014:role/GitHubActions-japanese-language-bot"
     }
 }
