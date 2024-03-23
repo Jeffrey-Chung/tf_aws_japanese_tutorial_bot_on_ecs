e3a481c976089eaec73dd1f3a75292b81f9415c2
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2024-03-24 03:00:54 +1100

    Use AWS CLI to create application version

--- modules/beanstalk/beanstalk.tf
+++ modules/beanstalk/beanstalk.tf
@@ -15,10 +15,3 @@ resource "aws_elastic_beanstalk_environment" "beanstalk_environment" {
     }
 }
 
-resource "aws_elastic_beanstalk_application_version" "japanese_language_bot_version" {
-  name        = var.beanstalk_version_label
-  application = var.beanstalk_application_name
-  description = var.beanstalk_application_description
-  bucket      = aws_s3_bucket.jchung_s3_bucket.id
-  key         = aws_s3_object.docker_compose_file.id
-}
