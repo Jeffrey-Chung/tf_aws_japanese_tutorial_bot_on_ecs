37443c8f50bad93066fb9fe10c4879ec9c2717f3
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2024-03-23 23:40:25 +1100

    add setting block to fix waiting for elastic beanstalk environment error

--- modules/beanstalk/beanstalk.tf
+++ modules/beanstalk/beanstalk.tf
@@ -15,4 +15,10 @@ resource "aws_elastic_beanstalk_environment" "beanstalk_environment" {
   name                = var.beanstalk_environment_name
   application         = var.beanstalk_application_name
   solution_stack_name = "64bit Amazon Linux 2023 v4.2.2 running Docker"
+
+  setting {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name      = "IamInstanceProfile"
+      value     = "aws-elasticbeanstalk-ec2-role"
+    }
 }
