7ad085a168259c0a0cf1d97e1d7dc4985b4c0157
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2024-04-09 00:01:04 +1000

    Add ecs definition and cluster

---
+++ ecs.tf
@@ -0,0 +1,28 @@
+resource "aws_ecs_cluster" "bot_ecs_cluster" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_ecs_task_definition" "bot_ecs_task" {
+  family                   = var.ecs_task_name # Name your task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": ${var.ecs_task_name},
+      "image": "${aws_ecr_repository.bot_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": ${var.container_and_host_port},
+          "hostPort": ${var.container_and_host_port}
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate for container orchestration
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory to be 512MB, required for Fargate
+  cpu                      = 256         # Specify the CPU to require 256MB, required for Fargate
+}
--- variables.tf
+++ variables.tf
@@ -10,3 +10,12 @@ variable "docker_file" {
 variable "ecr_repo_name" {
   default = "tf-aws-japanese-tutorial-bot-ecr-repo"
 }
+variable "ecs_cluster_name" {
+  default = "tf-aws-japanese-tutorial-bot-ecs-cluster"
+}
+variable "ecs_task_name" {
+  default = "tf-aws-japanese-tutorial-bot-ecs-task"
+}
+variable "container_and_host_port" {
+  default = "5000" # May be separated for container and host ports
+}
